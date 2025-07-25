9c4f2ff23ed5c3b57317d10aa69777bc2d0a635e
CodeSearcher6 <mkiil3852@gmail.com>
2025-07-25 19:03:31 +0300

    added feature Kick_Collider

---
+++ Assets/StarterAssets/ThirdPersonController/Scripts/Kick_Collider.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Kick_Collider : MonoBehaviour
+{
+    [SerializeField] private float kickForce = 25f;
+    [SerializeField] private ForceMode forceMode = ForceMode.Impulse; 
+    
+    
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Collision with: " + other.collider.name);
+        
+
+        Rigidbody rb = other.collider.GetComponent<Rigidbody>();
+
+        if (other.collider.CompareTag("Player") && rb != null)
+        {
+            rb.freezeRotation = true;
+        }
+
+        if (rb != null)
+        {
+            rb.AddForce(Vector3.up * kickForce, forceMode);
+            Debug.Log("Player collided with cube");
+        }
+
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.collider.CompareTag("Player"))
+        {
+            Debug.Log("Player exited cube collider");
+        }
+    }
+}
