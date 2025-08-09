be5c052e03aa7d372f288e3b561d1e0174fa0991
CodeSearcher6 <mkiil3852@gmail.com>
2025-08-09 14:10:49 +0300

    Animator Logic Remake

--- Assets/Input/PlayerController.cs
+++ Assets/Input/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using Dreamteck.Forever;
 using UnityEngine;
+
 namespace Game
 {
     public class PlayerController : MonoBehaviour
@@ -11,19 +12,37 @@ namespace Game
         [SerializeField] private float _joystickspeed = 2f;
         [SerializeField] private float _maxInputMagnitude = 3f;
         [SerializeField] private float _offsetSpeed = 2f;
-        [SerializeField] private Vector2 _targetVector;
         [SerializeField] private float _jumpForce = 5f;
         [SerializeField] private float _gravity = 9.8f;
+        [SerializeField] private Vector2 _targetVector;
+
         private float _verticalVelocity;
-        private const string IsRunning = "IsRunning";
-        private const string IsJumping = "IsJumping";
         private bool _isJumping;
         private const int LevelWidth = 4;
 
+        private enum AnimationState { Idle, Running, Jumping }
+        private AnimationState _currentState;
+
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+            _targetVector = Vector2.up;
+            _inputController = new();
+            _inputController.SubscribeEvents();
+            SubscribeEvents();
+        }
+
         private void Update()
+        {
+            HandleMovement();
+            HandleAnimationState();
+        }
+
+        private void HandleMovement()
         {
             Vector2 clampedInput = Vector2.ClampMagnitude(_targetVector, _maxInputMagnitude);
             Vector2 target = new Vector2(clampedInput.x * _joystickspeed, _basicRunner.motion.offset.y);
+
             if (_isJumping)
             {
                 _verticalVelocity -= _gravity * Time.deltaTime;
@@ -38,18 +57,37 @@ namespace Game
             }
 
             _basicRunner.motion.offset = Vector2.MoveTowards(_basicRunner.motion.offset, target, Time.deltaTime * _offsetSpeed);
-
-            bool isMoving = Mathf.Abs(_basicRunner.motion.offset.x) > 0.01f;
-            animator.SetBool(IsRunning, isMoving);
-
         }
-        private void Awake()
+
+        private void HandleAnimationState()
         {
-            animator = GetComponent<Animator>();
-            _targetVector = Vector2.up;
-            _inputController = new();
-            _inputController.SubscribeEvents();
-            SubscribeEvents();
+            bool isMoving = Mathf.Abs(_basicRunner.motion.offset.x) > 0.01f;
+
+            if (_isJumping)
+            {
+                SetAnimationState(AnimationState.Jumping);
+            }
+            else if (isMoving)
+            {
+                SetAnimationState(AnimationState.Running);
+            }
+        }
+
+        private void SetAnimationState(AnimationState newState)
+        {
+            if (newState == _currentState) return;
+
+            switch (newState)
+            {
+                case AnimationState.Running:
+                    animator.Play("Run");
+                    break;
+                case AnimationState.Jumping:
+                    animator.Play("Jump");
+                    break;
+            }
+
+            _currentState = newState;
         }
 
         public void SubscribeEvents()
@@ -57,7 +95,6 @@ namespace Game
             _inputController.MovementRecieved += OnMovementReceived;
             _inputController.MovementEnded += OnMovementEnded;
             _inputController.JumpPerformed += OnJumpPerformed;
-
         }
 
         private void UnsubscribeEvents()
@@ -65,7 +102,6 @@ namespace Game
             _inputController.MovementRecieved -= OnMovementReceived;
             _inputController.MovementEnded -= OnMovementEnded;
             _inputController.JumpPerformed -= OnJumpPerformed;
-
         }
 
         private void OnMovementReceived(Vector2 movement)
@@ -73,24 +109,25 @@ namespace Game
             Debug.Log("Movement received: " + movement);
             _targetVector = new Vector2(Mathf.Clamp(_targetVector.x + movement.x, -LevelWidth, LevelWidth), 2f);
         }
+
         private void OnMovementEnded()
         {
             _targetVector = Vector2.zero;
             Debug.Log("Movement ended");
         }
+
         private void OnJumpPerformed()
         {
             if (_isJumping) return;
+
             _verticalVelocity = _jumpForce;
             _isJumping = true;
-            animator.SetTrigger(IsJumping);
+            SetAnimationState(AnimationState.Jumping);
             Debug.Log("Jump triggered");
         }
 
         private void OnEscapeButtonPressed()
         {
-
-            // Handle escape button logic here
             Debug.Log("Escape button pressed");
         }
 
@@ -99,7 +136,5 @@ namespace Game
             UnsubscribeEvents();
             _inputController.Dispose();
         }
-
-
     }
 }
