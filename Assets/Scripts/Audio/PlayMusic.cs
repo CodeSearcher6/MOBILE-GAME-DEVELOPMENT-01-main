f985ee2bbbc1d47f93069f0f93f8f9c7074f4846
CodeSearcher6 <mkiil3852@gmail.com>
2025-08-20 16:30:46 +0300

    audio fix

--- Assets/Scripts/Audio/PlayMusic.cs
+++ Assets/Scripts/Audio/PlayMusic.cs
@@ -1,16 +1,15 @@
+using System.Threading.Tasks;
 using JSAM;
 using UnityEngine;
-using System.Threading.Tasks;
-
 
 public class PlayMusic : MonoBehaviour
-
 {
     private async void Start()
     {
-        await Task.Delay(1000); // Wait for 1 second before playing music
-
-        AudioManager.MasterVolume = 0.5f; // Set the master volume to 50%
-        var element = AudioManager.PlayMusic(AudioLibraryMusic.MusicFX, true); // Play the main menu music
+        await Task.Delay(1000); // Wait for 1 second before playing musicCollapse commentComment on line R9Vonk47 commented on Aug 18, 2025 Vonk47on Aug 18, 2025ContributorДодати очікування, писав в Slack як це зробитиWrite a replyCode has comments. Press enter to view.
+        var element = AudioManager.PlayMusic(AudioLibraryMusic.MusicFX, true);
+        AudioManager.OnAudioManagerInitialized();
+        AudioManager.MasterVolume = 0.5f; // Set the music volume to 50%
     }
+
 }
