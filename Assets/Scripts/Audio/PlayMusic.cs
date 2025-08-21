0f00efef18e39b8444e26dde8b06be858a4f1e93
CodeSearcher6 <mkiil3852@gmail.com>
2025-08-21 18:58:16 +0300

    audio subscription fixed

--- Assets/Scripts/Audio/PlayMusic.cs
+++ Assets/Scripts/Audio/PlayMusic.cs
@@ -1,15 +1,26 @@
-using System.Threading.Tasks;
-using JSAM;
 using UnityEngine;
+using JSAM;
 
 public class PlayMusic : MonoBehaviour
 {
-    private async void Start()
+    private void Awake()
     {
-        await Task.Delay(1000); // Wait for 1 second before playing musicCollapse commentComment on line R9Vonk47 commented on Aug 18, 2025 Vonk47on Aug 18, 2025ContributorДодати очікування, писав в Slack як це зробитиWrite a replyCode has comments. Press enter to view.
-        var element = AudioManager.PlayMusic(AudioLibraryMusic.MusicFX, true);
-        AudioManager.OnAudioManagerInitialized();
-        AudioManager.MasterVolume = 0.5f; // Set the music volume to 50%
+        AudioManager.OnAudioManagerInitialized += HandleAudioInitialized;
+        if (AudioManager.Instance.Initialized)
+        {
+            HandleAudioInitialized();
+        }
+    }
+    private void OnDestroy()
+    {
+        AudioManager.OnAudioManagerInitialized -= HandleAudioInitialized;
     }
 
+    private void HandleAudioInitialized()
+    {
+        Debug.Log("Music initialized");
+        AudioManager.PlayMusic(AudioLibraryMusic.MusicFX, true);
+        Debug.Log("Now playing: " + AudioLibraryMusic.MusicFX);
+
+    }
 }
